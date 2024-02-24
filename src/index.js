37f5fb1da9669956ecadd150b4312c5dfa711a9e
hrishd <hrishd@abv.bg>
2024-02-24 19:19:10 +0200

    added a file to register commands

--- src/index.js
+++ src/index.js
@@ -14,10 +14,6 @@ client.on('ready', (c) => {
     console.log(`${c.user.username} is online!`);
 });
 
-/* client.on('messageCreate', (msg) => {
-    console.log(`${msg.member.displayName}: ${msg.content}`);
-}); */
-
 client.on('messageCreate', (msg) => {
 
     //Dont do anything if the msg is sent by a bot
