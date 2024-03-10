ba797110ba392a271444b6d1eecb7cd7d8d6caaf
hrishd <hrishd@abv.bg>
2024-03-11 00:03:32 +0200

    print numbers

--- src/index.js
+++ src/index.js
@@ -24,19 +24,23 @@ client.on('messageCreate', (msg) => {
     if (msg.content === 'hello') {
         msg.reply('zdr kopele!');
     }
+
 });
 
 //Event listener for slash commands
 client.on('interactionCreate', (interaction) => {
-    
+
     //Check if the interaction is not a slash command
     if (!interaction.isChatInputCommand()) return;
 
     //Check for different commands
     if (interaction.commandName === 'hey') {
         interaction.reply("ko staa kopele!");
-    } else if (interaction.commandName === 'kopr') {
-        interaction.reply("nishto kopele!");
+    } else if (interaction.commandName === 'add') {
+        const num1 = interaction.options.get('num1');
+        const num2 = interaction.options.get('num2');
+
+        console.log(`${num1.value} ${num2.value}`);
     }
 });
 
